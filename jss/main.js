dfa5e845481bc14c159f54bb06970c773d68c6dc
Andrewchik <andriy.goryachiy16@gmail.com>
2020-10-13 15:57:08 +0300

    third commit list

--- jss/main.js
+++ jss/main.js
@@ -4,6 +4,12 @@ let todo = document.querySelector('.todo')
 
 let todoList = []
 
+
+if(localStorage.getItem('todo')){
+    todoList = JSON.parse(localStorage.getItem('todo'))
+    displayMessages()
+}
+
 addButton.addEventListener('click', function (params) {
 
         let newTodo = {
@@ -14,18 +20,20 @@ addButton.addEventListener('click', function (params) {
 
         todoList.push(newTodo)
         displayMessages()
+        localStorage.setItem('todo', JSON.stringify(todoList))
      
     });
 
     function displayMessages() {
         let displayMessage = '';
-        todoList.forEach((item, i) => {
+        todoList.forEach(function(item, i){
             displayMessage += `
             <li>
-                <input type = 'checkbox' id='item_${i}'>
+                <input type = 'checkbox' id='item_${i}' ${item.checked ? 'checked' : ''}>
                 <label for='item_${i}'>${item.todo}</label>
             </li>
             `
+            
            todo.innerHTML = displayMessage
         })
     }
