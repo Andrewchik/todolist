09579ade7541bf7db8681036ee4ee905b2f81fbb
Andrewchik <andriy.goryachiy16@gmail.com>
2020-10-13 15:19:58 +0300

    first commit list

---
+++ jss/main.js
@@ -0,0 +1,30 @@
+let addMessage = document.querySelector('.message')
+let addButton = document.querySelector('.add')
+let todo = document.querySelector('.todo')
+
+let todoList = []
+
+addButton.addEventListener('click', function (params) {
+
+        let newTodo = {
+            todo: addMessage.value,
+            checked: false,
+            important: false
+        };
+
+        todoList.push(newTodo)
+        displayMessages()
+     
+    });
+
+    function displayMessages() {
+        todoList.forEach((item, i) => {
+            let displayMessage = `
+            <li>
+                <input type = 'checkbox' id='item_${i}'>
+                <label for='item_${i}'>${item.todo}</label>
+            </li>
+            `
+           todo.innerHTML = displayMessage
+        })
+    }
