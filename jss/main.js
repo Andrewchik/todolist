e78ddc759c14598ea37ee519aa223891b70ee357
Andrewchik <andriy.goryachiy16@gmail.com>
2020-10-21 01:08:07 +0300

    checkbox size and other

--- jss/main.js
+++ jss/main.js
@@ -38,10 +38,20 @@ addButton.addEventListener('click', function () {
         todoList.forEach(function(item, i){
             displayMessage += `
             <li>
-                <input type = 'checkbox' id='item_${i}' ${item.checked ? 'checked' : ''}>
+                <input type = 'checkbox'  id='item_${i}' ${item.checked ? 'checked' : ''}>
                 <label for='item_${i}'>${item.todo}</label>
             </li>
+           
             <select size = "1">
+            <option>0:00</option>
+            <option>1:00</option>
+            <option>2:00</option>
+            <option>3:00</option>
+            <option>4:00</option>
+            <option>5:00</option>
+            <option>6:00</option>
+            <option>7:00</option>
+            <option>8:00</option>
 	            <option>9:00</option>
 	            <option>10:00</option>
 	            <option>11:00</option>
